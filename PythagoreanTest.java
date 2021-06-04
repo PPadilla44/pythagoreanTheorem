036b3f0d1dbecd7e1af8ec60abb5dd9e8c23eb03
Pablo Padilla <Padilla.Pablo1998@gmail.com>
2021-06-04 15:20:19 -0700

    removedd unncessary code

--- PythagoreanTest.java
+++ PythagoreanTest.java
@@ -1,16 +1,8 @@
 public class PythagoreanTest{
     public static void main(String[] args) {
-        Pythagorean test1 = new Pythagorean();
-        double result1 = test1.calculateHypotenuse(2,2);
-        System.out.println(result1);
-
-        Pythagorean test2 = new Pythagorean();
-        double result2 = test2.calculateHypotenuse(3,8);
-        System.out.println(result2);
-
-        Pythagorean test3 = new Pythagorean();
-        double result3 = test3.calculateHypotenuse(10,17);
-        System.out.println(result3);
-        
+        Pythagorean test = new Pythagorean();
+        System.out.println(test.calculateHypotenuse(2,2));
+        System.out.println(test.calculateHypotenuse(3,8));
+        System.out.println(test.calculateHypotenuse(10,17));
     }
 }
